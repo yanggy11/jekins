1ab5e9dbf9685d7d645823d23d46f5793fdb8361
yanggy <yangguiyun250725@163.com>
2018-10-31 11:45:39 +0800

    test

--- src/main/java/com/derrick/jekins/JekinsApplication.java
+++ src/main/java/com/derrick/jekins/JekinsApplication.java
@@ -2,11 +2,19 @@ package com.derrick.jekins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class JekinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JekinsApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "home page";
     }
 }

a0ba8e4c5d56adf398abaceb9706b9132f48091a
yanggy <yangguiyun250725@163.com>
2018-10-31 11:44:04 +0800

    base

---
+++ src/main/java/com/derrick/jekins/JekinsApplication.java
@@ -0,0 +1,12 @@
+package com.derrick.jekins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JekinsApplication.class, args);
+    }
+}
